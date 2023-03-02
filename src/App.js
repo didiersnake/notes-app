d77c6ed411a2796526a7d2e8e4b5f44768a368d2
didiersnake <didier.djakoua@gmail.com>
2023-03-02 15:09:13 +0100

    Editor setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React, {useState} from 'react';
 import './App.css';
 import SideBar from './components/SideBar';
+import Editor from './components/Editor'
 import { nanoid } from 'nanoid';
 
 function App() {
 
-      const [notes, setNotes] = useState([]);
-      const [currentNote, setCurrentNode] = useState("");
+    const [notes, setNotes] = useState([]);
+    const [currentNoteId, setCurrentNoteId] = useState(
+      (notes[0] && notes[0].id) || ""
+    );
 
-      
-      function creatNote() {
-        const newNode = { id: nanoid(), title: `note ${notes.length + 1}` };
-        setCurrentNode(newNode.id);
-        const result = setNotes((prevState) => {
-          return [...prevState, newNode];
-        });
-        return result;
-      }
-
-
+    function creatNote() {
+      const newNode = { id: nanoid(), body: "Enter your text..." };
+      setNotes((prevState) => {
+        return [...prevState, newNode];
+      });
+      setCurrentNoteId(newNode.id);
+    }
+  // set editor value from currentNote ID
+  function findCurrentNote() {
+    return notes.find((item) => {
+      return item.id === currentNoteId  
+    }) || notes[0] 
+  }
+  
+  // On currentNote ID value change, change the editor 
+  function updateNote(text) {
+    return setNotes((prevState) => notes.map((note) => 
+      note.id === currentNoteId ? { ...note, body: text }
+        : prevState))
+  }
 
   return (
     <div className="App">
       
-      <SideBar notes={notes} setCurrentNoteId={setCurrentNode}
-        currentNoteId={currentNote} creatNote={creatNote} />
+      <SideBar notes={notes} setCurrentNoteId={setCurrentNoteId}
+        currentNoteId={currentNoteId} creatNote={creatNote} />
+      
+      <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+      
     </div>
   );
 }
---
+++ src/components/Editor.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactMde from "react-mde";
+import Showdown from "showdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true
+});
+
+export default function Editor({currentNote ,updateNote}) {
+
+   const [selectedTab, setSelectedTab] = React.useState("write")
+
+    return (
+     <div className="container">
+       <ReactMde
+         value={currentNote}
+         onChange={updateNote}
+         selectedTab={selectedTab}
+         onTabChange={setSelectedTab}
+         generateMarkdownPreview={(markdown) =>
+            Promise.resolve(converter.makeHtml(markdown))
+        }
+            minEditorHeight={80}
+            heightUnits="vh"
+       />
+     </div>
+   );
+}
+
